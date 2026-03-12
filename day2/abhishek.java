1029d5190600bbb430831f4f8d9c6092f278e1ca
Ahishek Ranjan <aranjan7890@gmail.com>
2026-03-12 22:22:35 +0530

    da1, day2, day3

---
+++ day2/abhishek.java
@@ -0,0 +1,8 @@
+class Me 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Abhishek Ranjan");
+		System.out.println("City: Daltonganj");
+	}
+}
---
+++ day3/AccountInfo.java
@@ -0,0 +1,19 @@
+class AccountInfo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Account No. 93459398583");
+		System.out.println("Account Holder name: Abhishek Ranjan");
+		System.out.println("Branch code: 847509");
+		System.out.println("IFSC Code: 8945");
+	}
+}
+
+class AccountHolderDetails
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Abhishek Ranjan");
+		System.out.println("Gender: Male");
+	}
+}
