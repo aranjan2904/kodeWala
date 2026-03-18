8961900e609bdd6f968a7308ea98ca638c074be8
Ahishek Ranjan <aranjan7890@gmail.com>
2026-03-18 10:19:34 +0530

    day7 types of variable

---
+++ day7/Hospital.java
@@ -0,0 +1,16 @@
+class Hospital
+{
+	static String hospitalName = "MAX HOSPITAL";
+	static String hospitalAddress = "Delhi";
+	int patientID = 4234;
+	String patientName = "Nagesh";
+	int PatientAge = 98;
+	
+	
+	public static void main(String []args){
+		int roomNumber = 205;
+		System.out.println("Hospital Name= "+hospitalName);
+		System.out.println("Hospital Address =" + hospitalAddress);
+		System.out.println("Room Number = " + roomNumber);
+	}
+}
---
+++ day7/Hotel.java
@@ -0,0 +1,10 @@
+class Hotel
+{
+	static String name = "Mahi Hotel";
+	static int charge;
+	
+	public static void main(String []args){
+		System.out.println("Hotel name is = "+name);
+		System.out.println("Hotel charges are = "+ charge);
+	}
+}
