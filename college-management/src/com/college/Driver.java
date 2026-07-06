da4041f2639d0b6ffa301ea7ca3264c99677b441
Ahishek Ranjan <aranjan7890@gmail.com>
2026-07-06 10:37:01 +0530

    manual jar import and export practice

---
+++ college-management/src/com/college/Driver.java
@@ -0,0 +1,11 @@
+package com.college;
+
+import com.college.students.Students;
+
+public class Driver {
+	public static void main(String[] args) {
+		
+		Students student = new Students();
+		student.displayDetails();
+	}
+}
---
+++ student-management/src/com/college/students/Students.java
@@ -0,0 +1,14 @@
+package com.college.students;
+
+public class Students {
+	
+	public void displayDetails() {
+        System.out.println("Student ID : 101");
+        System.out.println("Name       : Abhishek Ranjan");
+        System.out.println("Age        : 21");
+        System.out.println("Course     : B.Tech");
+        System.out.println("Branch     : Computer Science");
+        System.out.println("CGPA       : 8.45");
+    }
+}
+
