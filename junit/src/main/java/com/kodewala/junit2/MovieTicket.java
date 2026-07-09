4022e8fdd738e9f4ca77e022222ef484a8982405
Ahishek Ranjan <aranjan7890@gmail.com>
2026-07-09 11:28:07 +0530

    junit practice

--- junit/src/main/java/com/kodewala/junit2/MovieTicket.java
+++ junit/src/main/java/com/kodewala/junit2/MovieTicket.java
@@ -1,5 +1,18 @@
 package com.kodewala.junit2;
 
+
 public class MovieTicket {
 
+    public double calculatePrice(double price, String category) {
+
+        if (category.equals("STUDENT")) {
+            price = price * 0.80;
+        } else if (category.equals("SENIOR")) {
+            price = price * 0.70;
+        } else if (category.equals("CHILD")) {
+            price = price * 0.50;
+        }
+
+        return price;
+    }
 }
--- junit/src/test/java/com/kodewala/junitTest2/MovieTicketTest.java
+++ junit/src/test/java/com/kodewala/junitTest2/MovieTicketTest.java
@@ -1,5 +1,35 @@
 package com.kodewala.junitTest2;
 
-public class MovieTicketTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import com.kodewala.junit2.MovieTicket;
+
+class MovieTicketTest {
+
+    MovieTicket ticket;
+
+    @BeforeEach
+    void setup() {
+        ticket = new MovieTicket();
+    }
+
+    @Test
+    void testStudentDiscount() {
+
+        assertEquals(400, ticket.calculatePrice(500, "STUDENT"));
+    }
+
+    @Test
+    void testSeniorDiscount() {
+
+        assertEquals(350, ticket.calculatePrice(500, "SENIOR"));
+    }
+
+    @Test
+    void testChildDiscount() {
+
+        assertEquals(250, ticket.calculatePrice(500, "CHILD"));
+    }
 }
